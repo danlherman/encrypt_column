44614b6eba684328d2bf77529edd013fbaa17e66
Dan Herman <dherman@globaldebtregistry.com>
2016-08-10 10:25:36 -0400

    added test for encrypt

--- lib/encrypt_column/encrypt.rb
+++ lib/encrypt_column/encrypt.rb
@@ -1,6 +1,6 @@
 class Encrypt
-  def text(plaintext)
-    raise EncryptionKeyConfigMissingError unless ENCRYPT_COLUMN_CONFIG[:encryption_key].present?
-    ActiveSupport::MessageEncryptor.new(ENCRYPT_COLUMN_CONFIG[:encryption_key]).encrypt_and_sign(plaintext)
+  def self.text(plaintext)
+    return raise 'Missing Encryption Key Config' if ENV['encryption_key'].nil?
+    ActiveSupport::MessageEncryptor.new(ENV['encryption_key']).encrypt_and_sign(plaintext)
   end
 end
---
+++ spec/encrypt_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'encrypt_column/encrypt'
+require 'active_support'
+
+describe Encrypt do
+  let(:plaintext) { 'plain text' }
+
+  subject { Encrypt.text(plaintext) }
+
+  context 'encryption key config specified' do
+
+    before do
+      ENV['encryption_key'] = '9438290832459034095840398509809sdsfkl;kdsgfl;kdfsgsdsdlsad'
+    end
+
+    it 'returns a encrypted value of a text string' do
+      expect(subject).to be_truthy
+    end
+  end
+
+  context 'too short encryption key specified' do
+    before do
+      ENV['encryption_key'] = 'shortkey'
+    end
+
+    it 'return a encryption key too short error' do
+      expect{subject}.to raise_error('key length too short')
+    end
+
+  end
+
+  context 'encryption key config specified' do
+
+    before do
+      ENV['encryption_key'] = nil
+    end
+
+    it 'return a encryption key missing config error' do
+      expect{subject}.to raise_error('Missing Encryption Key Config')
+    end
+  end
+end
+
