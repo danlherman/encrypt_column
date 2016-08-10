361264150de7e1972af7496f35a6e95674876fd9
Dan Herman <dherman@globaldebtregistry.com>
2016-08-10 10:03:07 -0400

    added tests for hashed

--- spec/encrypt_column_spec.rb
+++ spec/encrypt_column_spec.rb
@@ -5,7 +5,7 @@ describe EncryptColumn do
     expect(EncryptColumn::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  xit 'does something useful' do
     expect(false).to eq(true)
   end
 end
