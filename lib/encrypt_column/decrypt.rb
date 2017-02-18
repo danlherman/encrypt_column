4a9188d3be5e92cca9363671c71f63180378629c
Dan Herman <dherman@intratechs.com>
2017-02-18 16:26:55 -0500

    added tests for nil and blank and new error msg for missing ciphertext

--- lib/encrypt_column/decrypt.rb
+++ lib/encrypt_column/decrypt.rb
@@ -3,6 +3,7 @@ class Decrypt
     raise 'Encryption Key Config Missing' unless key.present?
     ActiveSupport::MessageEncryptor.new(key).decrypt_and_verify(ciphertext)
   rescue ActiveSupport::MessageVerifier::InvalidSignature
+    return 'ERROR: Missing encryption ciphertext' if ciphertext.nil? || ciphertext.blank?
     return 'ERROR: Wrong encryption key specified'
   end
 end
--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -4,9 +4,13 @@ require 'active_support'
 
 describe Decrypt do
   let(:plaintext) { 'plain text' }
+  let(:notext) { '' }
+  let(:niltext) { nil }
   let(:good_key) { '9438290832459034095840398509809sdsfkl;kdsgfl;kdfsgsdsdlsad' }
   let(:wrong_key) { 'xxx9438290832459034095840398509809sdsfkl;' }
   let(:ciphertext) { Encrypt.text(plaintext) }
+  let(:ciphernotext) { Encrypt.text(notext) }
+  let(:cipherniltext) { Encrypt.text(niltext) }
 
   context 'correct encryption key config specified' do
 
@@ -18,6 +22,21 @@ describe Decrypt do
 
     it 'returns the correct plaintext value' do
       expect(subject).to eq(plaintext)
+    end
+
+    it 'decrypts an empty string' do
+      subject = Decrypt.cipher(ciphernotext)
+      expect(subject).to eq(notext)
+    end
+
+    it 'decrypts a nil string' do
+      subject = Decrypt.cipher(cipherniltext)
+      expect(subject).to eq(niltext)
+    end
+
+    it 'has a blank ciphertext supplied' do
+      subject = Decrypt.cipher('')
+      expect(subject).to eq('ERROR: Missing encryption ciphertext')
     end
   end
 
